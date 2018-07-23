42d73abfcdfb4889120dae3d4aefbb78c7215790
fdemets <frederik.demets@gmail.com>
2018-07-23 11:09:36 +0200

    Initial Commit

---
+++ ShowpadSync/ShowpadSync/ShowpadSync.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace ShowpadSync
+{
+    partial class ShowpadSync
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShowpadSync));
+            this.spFileWatcher = new System.IO.FileSystemWatcher();
+            this.spFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnFolderSelect = new System.Windows.Forms.Button();
+            this.txtClientID = new System.Windows.Forms.TextBox();
+            this.txtSecret = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtOrganization = new System.Windows.Forms.TextBox();
+            this.lblOrganization = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chkAutoStart = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.spFileWatcher)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spFileWatcher
+            // 
+            this.spFileWatcher.SynchronizingObject = this;
+            this.spFileWatcher.Created += new System.IO.FileSystemEventHandler(this.spFileWatcher_Created);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkAutoStart);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnFolderSelect);
+            this.groupBox1.Controls.Add(this.txtFolder);
+            this.groupBox1.Location = new System.Drawing.Point(27, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(713, 118);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.lblOrganization);
+            this.groupBox2.Controls.Add(this.txtOrganization);
+            this.groupBox2.Controls.Add(this.txtPassword);
+            this.groupBox2.Controls.Add(this.txtUser);
+            this.groupBox2.Controls.Add(this.txtSecret);
+            this.groupBox2.Controls.Add(this.txtClientID);
+            this.groupBox2.Location = new System.Drawing.Point(27, 166);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(713, 218);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Showpad credentials";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(404, 422);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(177, 51);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(219, 422);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(179, 51);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Enabled = false;
+            this.txtFolder.Location = new System.Drawing.Point(192, 32);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(434, 31);
+            this.txtFolder.TabIndex = 4;
+            // 
+            // btnFolderSelect
+            // 
+            this.btnFolderSelect.Location = new System.Drawing.Point(632, 32);
+            this.btnFolderSelect.Name = "btnFolderSelect";
+            this.btnFolderSelect.Size = new System.Drawing.Size(75, 39);
+            this.btnFolderSelect.TabIndex = 5;
+            this.btnFolderSelect.Text = "...";
+            this.btnFolderSelect.UseVisualStyleBackColor = true;
+            this.btnFolderSelect.Click += new System.EventHandler(this.btnFolderSelect_Click);
+            // 
+            // txtClientID
+            // 
+            this.txtClientID.Location = new System.Drawing.Point(192, 65);
+            this.txtClientID.Name = "txtClientID";
+            this.txtClientID.Size = new System.Drawing.Size(515, 31);
+            this.txtClientID.TabIndex = 5;
+            // 
+            // txtSecret
+            // 
+            this.txtSecret.Location = new System.Drawing.Point(192, 102);
+            this.txtSecret.Name = "txtSecret";
+            this.txtSecret.Size = new System.Drawing.Size(515, 31);
+            this.txtSecret.TabIndex = 6;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(192, 139);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(515, 31);
+            this.txtUser.TabIndex = 7;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(192, 176);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(515, 31);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtOrganization
+            // 
+            this.txtOrganization.Location = new System.Drawing.Point(192, 28);
+            this.txtOrganization.Name = "txtOrganization";
+            this.txtOrganization.Size = new System.Drawing.Size(515, 31);
+            this.txtOrganization.TabIndex = 9;
+            // 
+            // lblOrganization
+            // 
+            this.lblOrganization.AutoSize = true;
+            this.lblOrganization.Location = new System.Drawing.Point(20, 31);
+            this.lblOrganization.Name = "lblOrganization";
+            this.lblOrganization.Size = new System.Drawing.Size(134, 25);
+            this.lblOrganization.TabIndex = 10;
+            this.lblOrganization.Text = "Organization";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Client ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Secret";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Password";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemStart,
+            this.toolStripMenuItemStop,
+            this.toolStripMenuItemQuit});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(139, 112);
+            // 
+            // toolStripMenuItemStart
+            // 
+            this.toolStripMenuItemStart.Name = "toolStripMenuItemStart";
+            this.toolStripMenuItemStart.Size = new System.Drawing.Size(138, 36);
+            this.toolStripMenuItemStart.Text = "Start";
+            // 
+            // toolStripMenuItemStop
+            // 
+            this.toolStripMenuItemStop.Name = "toolStripMenuItemStop";
+            this.toolStripMenuItemStop.Size = new System.Drawing.Size(138, 36);
+            this.toolStripMenuItemStop.Text = "Stop";
+            // 
+            // toolStripMenuItemQuit
+            // 
+            this.toolStripMenuItemQuit.Name = "toolStripMenuItemQuit";
+            this.toolStripMenuItemQuit.Size = new System.Drawing.Size(138, 36);
+            this.toolStripMenuItemQuit.Text = "Quit";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(781, 22);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(595, 451);
+            this.txtLog.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Folder to watch";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Auto start";
+            // 
+            // chkAutoStart
+            // 
+            this.chkAutoStart.AutoSize = true;
+            this.chkAutoStart.Location = new System.Drawing.Point(192, 80);
+            this.chkAutoStart.Name = "chkAutoStart";
+            this.chkAutoStart.Size = new System.Drawing.Size(28, 27);
+            this.chkAutoStart.TabIndex = 16;
+            this.chkAutoStart.UseVisualStyleBackColor = true;
+            // 
+            // ShowpadSync
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 506);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ShowpadSync";
+            this.Text = "ShowpadSync";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ShowpadSync_FormClosing);
+            this.Load += new System.EventHandler(this.ShowpadSync_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.spFileWatcher)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher spFileWatcher;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFolderSelect;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.FolderBrowserDialog spFolderBrowser;
+        private System.Windows.Forms.TextBox txtOrganization;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtSecret;
+        private System.Windows.Forms.TextBox txtClientID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOrganization;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemStart;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemStop;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemQuit;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkAutoStart;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ShowpadSync/ShowpadSync/ShowpadSync.cs
@@ -0,0 +1,209 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace ShowpadSync
+{
+    public partial class ShowpadSync : Form
+    {
+        HttpClient client;
+        string uploadTarget;
+        string myUploadsId;
+        public ShowpadSync()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFolderSelect_Click(object sender, EventArgs e)
+        {
+           if( spFolderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                spFileWatcher.Path = spFolderBrowser.SelectedPath;
+                txtFolder.Text = spFileWatcher.Path;
+            }
+        }
+
+        private void spFileWatcher_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            // File is created - upload to Showpad My Files
+
+            if (e.Name.Contains("jpg"))
+            {
+                client = new HttpClient();
+                client.BaseAddress = new Uri("https://" + txtOrganization.Text + ".showpad.biz");
+                var byteArray = new UTF8Encoding().GetBytes(txtClientID.Text + ":" + txtSecret.Text);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                Dictionary<string, string> tokenDetails = null;
+
+                var login = new Dictionary<string, string>
+            {
+           {"grant_type", "password"},
+           {"username", txtUser.Text},
+           {"password", txtPassword.Text},
+            };
+                var response = client.PostAsync("/api/v3/oauth2/token", new FormUrlEncodedContent(login)).Result;
+                AddToLog(response.ToString());
+
+                if (response.IsSuccessStatusCode)
+                {
+                    tokenDetails = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content.ReadAsStringAsync().Result);
+                    AddToLog(response.Content.ReadAsStringAsync().Result);
+                    if (tokenDetails != null && tokenDetails.Any())
+                    {
+                        var tokenNo = tokenDetails.FirstOrDefault().Value;
+                        client.DefaultRequestHeaders.Remove("Authorization");
+                        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenNo);
+
+
+                        // upload file
+
+                        MultipartFormDataContent form = new MultipartFormDataContent();
+
+                          form.Add(new StringContent("{\"materialisedPath\":\"/\"}"), "postProcessingInstructions");
+
+
+
+                        FileStream fs = File.OpenRead(e.FullPath);
+                        var streamContent = new StreamContent(fs);
+
+                        var imageContent = new ByteArrayContent(streamContent.ReadAsByteArrayAsync().Result);
+                        imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+
+                        form.Add(imageContent, "file", Path.GetFileName(e.FullPath));
+                        
+
+                       // response = client.PostAsync("/api/v3/divisions/"+uploadTarget+"/assets.json", form).Result;
+                        response = client.PostAsync("/api/v3/divisions/mine/assets.json", form).Result;
+
+                        
+
+                        dynamic resp = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+                          Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                        AddToLog(response.Content.ReadAsStringAsync().Result);
+                      //  string userid = resp.response.items[0].id;
+
+                      
+
+                    }
+                }
+
+            }
+        }
+
+       
+
+        void AddToLog(string t)
+        {
+            txtLog.Text = txtLog.Text + Environment.NewLine + t;
+        }
+        private void CreateClient()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://" + txtOrganization.Text + ".showpad.biz");
+            var byteArray = new UTF8Encoding().GetBytes(txtClientID.Text +":"+txtSecret.Text);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            Dictionary<string, string> tokenDetails = null;
+          
+            var login = new Dictionary<string, string>
+            {
+           {"grant_type", "password"},
+           {"username", txtUser.Text},
+           {"password", txtPassword.Text},
+            };
+            var response = client.PostAsync("/api/v3/oauth2/token", new FormUrlEncodedContent(login)).Result;
+          // AddToLog( response.ToString());
+
+            if (response.IsSuccessStatusCode)
+            {
+                tokenDetails = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content.ReadAsStringAsync().Result);
+              //  AddToLog(response.Content.ReadAsStringAsync().Result);
+                if (tokenDetails != null && tokenDetails.Any())
+                {
+                    var tokenNo = tokenDetails.FirstOrDefault().Value;
+                    client.DefaultRequestHeaders.Remove("Authorization");
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenNo);
+
+
+                    // get userid  c64ac7472f03f86a53e0b6759dffa914
+                    response = client.GetAsync("/api/v3/users.json?email="+HttpUtility.UrlEncode(txtUser.Text)).Result;
+                     dynamic resp = JsonConvert.DeserializeObject<dynamic>( response.Content.ReadAsStringAsync().Result);
+                  //  Console.WriteLine(userid.response.items[0]);
+                    string userid= resp.response.items[0].id;
+
+
+                    // get users myuploads id 7a21881d8d1195e8003d367a6dcfe7a3
+                    response = client.GetAsync("/api/v3/users/"+ userid + ".json?fields=myUploadsCollectionId").Result;
+                     resp = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+                //  Console.WriteLine(userid.response.items[0]);
+                     myUploadsId = resp.response.myUploadsCollectionId;
+
+                    // get my uploads target 77452d0026289d3901330871a6543a15
+
+                    response = client.GetAsync("/api/v3/users/" + userid + "/channels.json?name=My%20Channel&personalContentIncluded=true").Result;
+                    resp = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+               //       Console.WriteLine(resp.response.items[0].id);
+                     uploadTarget = resp.response.items[0].id;
+
+
+                }
+            }
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateClient();
+        }
+
+        private void ShowpadSync_Load(object sender, EventArgs e)
+        {
+            txtOrganization.Text = Properties.Settings.Default.Organization;
+            txtUser.Text = Properties.Settings.Default.Username;
+            txtPassword.Text = Properties.Settings.Default.Password;
+            txtClientID.Text = Properties.Settings.Default.ClientID;
+            txtSecret.Text = Properties.Settings.Default.Secret;
+            txtFolder.Text = Properties.Settings.Default.Folder;
+            chkAutoStart.Checked = Properties.Settings.Default.AutoStart;
+        }
+
+        private void ShowpadSync_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Organization = txtOrganization.Text;
+            Properties.Settings.Default.Username = txtUser.Text;
+            Properties.Settings.Default.Password = txtPassword.Text;
+            Properties.Settings.Default.ClientID = txtClientID.Text;
+            Properties.Settings.Default.Secret = txtSecret.Text;
+            Properties.Settings.Default.Folder = txtFolder.Text;
+            Properties.Settings.Default.AutoStart = chkAutoStart.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           // CreateClient();
+            spFileWatcher.Path = txtFolder.Text;
+            spFileWatcher.EnableRaisingEvents = true;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            spFileWatcher.EnableRaisingEvents = false;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+}
